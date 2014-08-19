7002545ae74e6a571c7a10338e437bf0e82cebac
asmith30 <asmith@southborough.kingston.sch.uk>
2014-08-19 17:45:41 +0100

    Running robot/chess.py

--- robot/chess.py
+++ robot/chess.py
@@ -6,4 +6,17 @@ from tealight.robot import (move,
                             left_side, 
                             right_side)
 
-# Add your code here
+def line:
+  move(32)
+# function to do line row line row
+def snaker:
+  line()
+  turn(1)
+  move(4)
+  turn(1)
+  line()
+  turn(-1)
+  move(4)
+  turn(-1)
+
+snaker()
