bfc5fb4df2d339fa055dfba050112dd49f8fd2df
asmith30 <asmith@southborough.kingston.sch.uk>
2014-08-19 18:17:58 +0100

    Running robot/cleanup.py

--- robot/cleanup.py
+++ robot/cleanup.py
@@ -7,10 +7,28 @@ from tealight.robot import (move,
                             right_side)
 
 # Add your code here def line(dir):
+def turner(dir):
+  dir = dir*-1
+  turn(dir)
+  move()
+  turn(dir)
 
-xvar = touch()
-print xvar
-xvar = left_side()
-print xvar
-xvar = right_side()
-print xvar
+def go(dir):
+  while touch() is fruit
+    move()
+  
+  if dir ==1:
+    while left_side() is fruit
+      move()
+
+  if dir ==-1:
+    while right_side is fruit
+      move()
+  turner(dir)  
+    
+
+
+dir = 1
+go(dir)
+
+
