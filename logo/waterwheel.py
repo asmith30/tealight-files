c6968ba4eda8985da5d06859cbcd146954d85c1c
asmith30 <asmith@southborough.kingston.sch.uk>
2014-08-19 14:02:39 +0100

    Running logo/waterwheel.py

--- logo/waterwheel.py
+++ logo/waterwheel.py
@@ -8,15 +8,15 @@ def square(side):
 def triangle (side):
   for i in range(0,3):
     move(side)
-    turn (60)
+    turn (120)
 
 def waterwheel(edges, size):
   angle = 360 / edges
   decoration = size / 2
   for i in range(0, edges):
     move(size)
-    #triangle(decoration)
-    square(decoration)
+    triangle(decoration)
+    #square(decoration)
     turn(angle)
 
 turn(90)
