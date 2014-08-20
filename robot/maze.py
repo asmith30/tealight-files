ce3e1d1f07c938b0463f800a68f1c3ff8f46fb03
asmith30 <asmith@southborough.kingston.sch.uk>
2014-08-20 17:06:26 +0100

    Running robot/maze.py

--- robot/maze.py
+++ robot/maze.py
@@ -5,10 +5,36 @@ from tealight.robot import (move,
                             smell, 
                             left_side, 
                             right_side)
+import random
 
 # Add your code here:
 
 for i in range(1,1500):
-  print touch()
-  move()
+  if touch() == 'None':
+    if right_side() == 'None':
+      if left_side() == 'None':
+        turn(random.randint(-1,1)
+        move()
+      else:
+        turn(random.randint(0,1)
+        move()
+    elif left_side() =='None':
+      turn (random.randint(-1,0)
+      move()
+    else:
+      move()
+  elif right_side() =='None':
+    if left_side() =='None':
+      turn (random.randint(0,1)*2-1)
+      move()
+    else:
+      turn(1)
+      move()
+  else:
+    turn(-1)
+    move()
+    
+       
+    move()
+  elif 
   
