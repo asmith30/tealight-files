be20b5870c08cfead7be274b0a01675c787ff618
asmith30 <asmith@southborough.kingston.sch.uk>
2014-08-20 17:40:34 +0100

    Running robot/maze.py

--- robot/maze.py
+++ robot/maze.py
@@ -23,26 +23,26 @@ for i in range(1,1500):
     print 'ahead and right'
     turn(random.randint(0,1))
     move()
-  if a== 'none' & r == 'wall' & l ==  'None':
+  if a== 'none' and r == 'wall' and l ==  'None':
     print 'ahead and left'
     turn (random.randint(-1,0))
     move()
-  if a == 'None' & r =='wall' & l ==  'wall':
+  if a == 'None' and r =='wall' and l ==  'wall':
     print 'ahead only'
     move()
-  if a == 'wall' & r == 'None' & l == 'None' :
+  if a == 'wall' and r == 'None' and l == 'None' :
     print 'left and right only'
     turn (random.randint(0,1)*2-1)
     move()
-  if a == 'wall' & r == 'None' & l == 'wall' :
+  if a == 'wall' and r == 'None' and l == 'wall' :
     print 'right only'
     turn(1)
     move()
-  if a == 'wall' & r == 'wall' & l == 'None' :
+  if a == 'wall' and r == 'wall' and l == 'None' :
     print'left only'
     turn(-1)
     move()
-  if a == 'wall' & r == 'wall' & l == 'wall' :
+  if a == 'wall' and r == 'wall' and l == 'wall' :
     print 'dead end'
     turn(2)
     move()
