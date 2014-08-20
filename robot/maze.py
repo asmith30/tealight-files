8504e1314ccf3a6eee7262e26f6ac36c850dc395
asmith30 <asmith@southborough.kingston.sch.uk>
2014-08-20 17:07:10 +0100

    Running robot/maze.py

--- robot/maze.py
+++ robot/maze.py
@@ -13,13 +13,13 @@ for i in range(1,1500):
   if touch() == 'None':
     if right_side() == 'None':
       if left_side() == 'None':
-        turn(random.randint(-1,1)
+        turn(random.randint(-1,1))
         move()
       else:
-        turn(random.randint(0,1)
+        turn(random.randint(0,1))
         move()
     elif left_side() =='None':
-      turn (random.randint(-1,0)
+      turn (random.randint(-1,0))
       move()
     else:
       move()
