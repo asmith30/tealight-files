294e5b8bf000d7270e295565afbc0e06d5fbc063
asmith30 <asmith@southborough.kingston.sch.uk>
2014-08-20 17:33:18 +0100

    Running robot/maze.py

--- robot/maze.py
+++ robot/maze.py
@@ -10,40 +10,42 @@ import random
 # Add your code here:
 
 for i in range(1,1500):
-  print touch()
-  print left_side()
-  print right_side()
-  if touch() == 'None':
-    if right_side() == 'None':
-      if left_side() == 'None':
-        print 'three way'
-        turn(random.randint(-1,1))
-        move()
-      else:
-        print 'ahead and right'
-        turn(random.randint(0,1))
-        move()
-    elif left_side() =='None':
-      print 'ahead and left'
-      turn (random.randint(-1,0))
-      move()
-    else:
-      print 'ahead only'
-      move()
-  elif right_side() =='None':
-    if left_side() =='None':
-      print 'left and right only'
-      turn (random.randint(0,1)*2-1)
-      move()
-    else:
-      print 'right only'
-      turn(1)
-      move()
-  else:
+  a = touch()
+  l = left_side()
+  r = right_side()  
+  print a + ' ' + l + ' ' +r
+ 
+  if a == 'None'& r == 'None' & l == 'None' :
+    print 'three way'
+    turn(random.randint(-1,1))
+    move()
+  if a=='None' & r == 'None' & l == 'wall' :
+    print 'ahead and right'
+    turn(random.randint(0,1))
+    move()
+  if a== 'none' & r == 'wall' & l ==  'None':
+    print 'ahead and left'
+    turn (random.randint(-1,0))
+    move()
+  if a == 'None' & r =='wall' & l ==  'wall':
+    print 'ahead only'
+    move()
+  if a == 'wall' & r == 'None' & l == 'None' :
+    print 'left and right only'
+    turn (random.randint(0,1)*2-1)
+    move()
+  if a == 'wall' & r == 'None' & l == 'wall' :
+    print 'right only'
+    turn(1)
+    move()
+  if a == 'wall' & r == 'wall' & l == 'None' :
     print'left only'
     turn(-1)
     move()
-    
+  if a == 'wall' & r == 'wall' & l == 'wall' :
+    print 'dead end'
+    turn(2)
+    move()
        
 
   
