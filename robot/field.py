4a929b2ed670ef6d765103f4b564233f772ae4cf
asmith30 <asmith@southborough.kingston.sch.uk>
2014-08-20 16:01:23 +0100

    Running robot/field.py

--- robot/field.py
+++ robot/field.py
@@ -7,6 +7,16 @@ from tealight.robot import (move,
                             right_side)
 
 # Add your code here
-for i in range(0,9):
+def spiral():
+  for i in range (1,40):
+    for j in range (1,i):
+     move()
+    turn (-1)
+    for j in range (1,i):
+      move()
+    turn(-1)
+    
+for i in range(0,20):
   print smell()
   move ()
+spiral()
