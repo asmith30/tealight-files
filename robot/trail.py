9924f44e25529acb7add94c3f3b01e33adffa8f4
asmith30 <asmith@southborough.kingston.sch.uk>
2014-08-21 21:41:39 +0100

    Running robot/trail.py

--- robot/trail.py
+++ robot/trail.py
@@ -7,3 +7,31 @@ from tealight.robot import (move,
                             right_side)
 
 # Add your code here
+def tri():
+  move()
+  a =str(touch())
+  l=str(left_side())
+  r=str(right_side())
+  if l=='fruit':
+    return -1
+  elif r=='fruit':
+    return 1
+  else:
+    print 'oh no im stuck'
+    return(0)
+for i in range (0,400)
+  a =str(touch())
+  l=str(left_side())
+  r=str(right_side())
+  if a =='fruit':
+    move()
+  elif r=='fruit':
+    turn (1)
+    move()
+  elif l=='fruit':
+    turn(-1)
+    move()
+  else:
+    turn(tri())
+    move()
+  
