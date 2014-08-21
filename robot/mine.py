8361733f2eedfd3b5db08af06731af622044e9f9
asmith30 <asmith@southborough.kingston.sch.uk>
2014-08-21 14:58:16 +0100

    Running robot/mine.py

--- robot/mine.py
+++ robot/mine.py
@@ -7,19 +7,19 @@ from tealight.robot import (move,
                             right_side)
 
 # Add your code here
-for i in range (1,6):
-a = str(touch())
-l=str(left_side())
-r=str(right_side())
-lasta ='fruit'
-lastl='fruit'
-lastr='fruit'
+for i in range (1,400):
+  a = str(touch())
+  l=str(left_side())
+  r=str(right_side())
+  lasta ='fruit'
+  lastl='fruit'
+  lastr='fruit'
 
-if a=='fruit':
-elif l=='fruit':
-  turn(-1)
-elif r=='fruit':
-  turn(1)
-else:
-  turn(2)
-move()
+  if a=='fruit':
+  elif l=='fruit':
+    turn(-1)
+  elif r=='fruit':
+    turn(1)
+  else:
+    turn(2)
+  move()
