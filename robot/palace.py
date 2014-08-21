ffe30048d42861dbe997dc7c26c233246aa1152c
asmith30 <asmith@southborough.kingston.sch.uk>
2014-08-21 18:29:30 +0100

    Running robot/palace.py

--- robot/palace.py
+++ robot/palace.py
@@ -7,7 +7,53 @@ from tealight.robot import (move,
                             right_side)
 
 # Add your code here
-for i in range(0,40):
+def ur():
+  turn(-1)
+  move()
+  turn(1)
+  move()
+  
+def dr():
+  turn(1)
+  move()
+  turn(-1)
+  move()
+def ul():
+
+def trythisway():
+  move()
+  if left_side()=='fruit':
+    return -1
+  elif right_side()=='fruit':
+   return 1
+  else:
+     turn(2)
+     move()
+     return(0)
+  
+def find():
+  while touch()!='fruit' and left_side()!='fruit' and right_side()!='fruit':
+   if look()=='fruit':
+      move()
+   else:
+      turn(-1)
+  move()
+  
+def traverse():
+  d = trythisway()
+  if d<>0:
+    turn(d)
+    move()
+    turn(d*-1)
+  else:
+    traverse()
+    
+  
+find()  
+for i in range(0,400):
+  a=str(touch())
+  l=str(left_side())
+  r=str(right_side())
   print look()
   print smell()
   print touch()
